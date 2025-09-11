1da59850d9c3378bb4e350e1c04a1055c3e564d4
FerDelbo <fedelbone@gmail.com>
2025-09-11 09:13:03 -0300

    Criando agente ReAct com wikipedia

--- ReAct/ReasonAct.py
+++ ReAct/ReasonAct.py
@@ -3,20 +3,23 @@ import os
 from dotenv import load_dotenv
 # Meu modelo llm (Gemini)
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
 # Criação do ReAct
 from langgraph.prebuilt import create_react_agent
 from langchain_community.agent_toolkits.load_tools import load_tools
 
+# Carregar a chave da API do arquivo .env
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=api_key)
+# Configurar o modelo LLM (Gemini)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro", temperature=0.7, google_api_key=api_key)
+# Kit de ferramentas (ReAct)
 tools = load_tools(["wikipedia"])
-
+# Criar o agente ReAct
 agent = create_react_agent(llm, tools)
 
-response = agent.invoke({
-    "message": [("human", "Quantas pessoas vivem no Brasil?")]
-})
-
-print(response.content)
+# Requsição ao agente
+response = agent.invoke({"messages":"Quem é o presidente do Brasil e qual a sua idade?"})
+# Exibir a resposta
+print(response["messages"][-1].content)
